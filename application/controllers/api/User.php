056d1297133426f65bf4a3f5e8267a3f96cca66b
agabru <agabru@vavisa-kw.com>
2019-06-21 23:53:14 +0300

    To test -m command with -a

--- application/controllers/api/User.php
+++ application/controllers/api/User.php
@@ -98,12 +98,14 @@ class User extends REST_Controller {
         if ( ! $this->upload->do_upload('userfile'))
         {
             $error = array('error' => $this->upload->display_errors());
+
+            $this->load->view('upload_form', $error);
         }
         else
         {
             $data = array('upload_data' => $this->upload->data());
 
-            $this->load->view('upload_success', $data);
+            //$this->load->view('upload_success', $data);
         }
     }
 }
